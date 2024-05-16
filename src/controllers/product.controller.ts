18882b0b313522cd71857417b86425d42aab4f08
juanesrp <juanesrp@gmail.com>
2024-05-16 16:09:49 -0500

    Commit para realizar deploy del backend

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { catchedController } from "../utils/catchedController";
+import {
+  getProductsService,
+  getProductByIdService,
+} from "../services/products.service";
+
+export const getProducts = catchedController(
+  async (req: Request, res: Response) => {
+    const products = await getProductsService();
+    res.json(products);
+  }
+);
+
+export const getProductById = catchedController(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const product = await getProductByIdService(Number(id));
+    res.json(product);
+  }
+);
---
+++ src/middlewares/userRegister.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { checkUserExists } from "../services/user.service";
+import { ClientError } from "../utils/errors";
+
+const validateUserRegister = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password, name, address, phone } = req.body;
+  console.log(email, password, name, address, phone);
+
+  if (!email || !password || !name || !address || !phone)
+    next(new ClientError("Missing fields"));
+  else next();
+};
+
+const validateUserExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+  if (await checkUserExists(email))
+    next(new ClientError("User already exists", 400));
+  else next();
+};
+
+export default [validateUserRegister, validateUserExists];
