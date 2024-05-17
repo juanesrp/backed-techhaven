79a7a5c6d4487f34983a87c6c8b0a1f1d5d3438a
juanesrp <juanesrp@gmail.com>
2024-05-16 19:32:57 -0500

    Eliminacion de variables de entorno que deje de usar al utilizar database_URL

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,12 +1,5 @@
 import { DataSource } from "typeorm";
-import {
-  DATABASE_URL,
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_USER,
-} from "./envs";
+import { DATABASE_URL } from "./envs";
 import { User } from "../entities/User";
 import { Credential } from "../entities/Credential";
 import { Order } from "../entities/Order";
@@ -16,12 +9,6 @@ import { Product } from "../entities/Product";
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: DATABASE_URL,
-
-  // host: DB_HOST,
-  // port: DB_PORT,
-  // username: DB_USER,
-  // password: DB_PASSWORD || "admin",
-  // database: DB_NAME,
   synchronize: true,
   // dropSchema: true,
   logging: false,
