060a277bae4a250bd7cb6849af5ad260515f1260
juanesrp <juanesrp@gmail.com>
2024-05-16 20:29:32 -0500

    Borro el puerto ya que se supone que vercel me debe de pasar uno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT: number = Number(process.env.PORT) || 3000;
+export const PORT: number = Number(process.env.PORT) || 3001;
 export const DATABASE_URL: string = process.env.DATABASE_URL || "";
 export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
