3a6e7412f6ce3400cf6d3d311d72209a3108bb12
juanesrp <juanesrp@gmail.com>
2024-05-16 19:22:41 -0500

    Agrego un try catch a la inicializacion del proyecto para que me muestre el error

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import { PORT } from "./config/envs";
+import { PORT, DATABASE_URL } from "./config/envs";
 import app from "./server";
-import "reflect-metadata"
+import "reflect-metadata";
 import { AppDataSource } from "./config/dataSource";
 import { preLoadCategories } from "./helpers/preLoadCategories";
 import { preLoadProducts } from "./helpers/preLoadProducts";
 
 const initialize = async () => {
+  try {
     console.log("Initializing server");
     await AppDataSource.initialize();
     console.log("Database initialized");
     await preLoadCategories();
     await preLoadProducts();
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
-}
+  } catch (error) {
+    console.error("Error initializing server:", error);
+  }
+};
 
 initialize();
-
